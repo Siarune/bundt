ff57badc48673f5183177d17d8413722d3627cf9
Aidan <enigma0fox@gmail.com>
2022-05-14 12:34:48 -0400

    search upgrade start

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,57 @@
-let searchBox=document.getElementById("searchtxt");
+const searchBox = document.getElementById("searchtxt");
+const get = document.getElementById("phpaction");
 
 searchBox.addEventListener('keypress', (e) => {
-    if(e.key || e.keyCode == 13){
-        setTimeout(()=>{searchBox.value=""},1000);
+    // if (e.code == "Enter") {
+    //     setTimeout(() => {searchBox.value=""}, 1000);
+    // }
+
+    switch (e.code) {
+        case 'Enter':
+            setTimeout(() => {searchBox.value=""}, 250);
+            break;
+        case 'Backslash':
+            setTimeout(() => {searchBox.value=""}, 250);
+            break;
     }
 });
 
+
+//commands
+//currently only to switch search engine, but will likely add other stuff like changing the layout
 searchBox.addEventListener('keydown', (e) => {
-    if (searchBox.value=="githu" && e.key=="b") {
-        window.open('https://www.github.com',"_self");
-    }
+        switch (e.key) {
+            case '`g':
+                setTimeout(() => {searchBox.value=""}, 500);
+                searchBox.placeholder = "Search for anything on Google";
+                get.action = "https://www.google.com/";
+                break;
+
+            case '`d':
+                setTimeout(() => {searchBox.value=""}, 500);
+                searchBox.placeholder = "Search for anything on DuckDuckgo";
+                get.action = "https://www.duckduckgo.com/";
+                break;
+
+            // case '`w':
+            //     setTimeout(() => {searchBox.value=""}, 500);
+            //     searchBox.placeholder = "Search for anything on Wikipedia";
+            //     get.action = "https://www.wikipedia.org/wiki";
+            //     searchBox.name = "";
+            //     break;
+
+            case '`s':
+                setTimeout(() => {searchBox.value=""}, 500);
+                searchBox.placeholder = "Search for anything on Startpage";
+                get.action = "https://www.startpage.com/sp/search";
+                break;
+
+            case '`l':
+                setTimeout(() => {searchBox.value=""}, 500);
+
+                case 'bento':
+                    CONFIG.bentoLayout = "bento";
+                    
+                
+        }
 });
