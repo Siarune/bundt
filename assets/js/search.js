dc7502f6b51b91eddee2982ef605b6032efedd22
Aidan Sharp <sia@siarune.dev>
2022-06-14 19:52:49 -0400

    icons and search command rework

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,57 +1,90 @@
 const searchBox = document.getElementById("searchtxt");
 const get = document.getElementById("phpaction");
 
+//Command controller (paste-safe)
 searchBox.addEventListener('keypress', (e) => {
-    // if (e.code == "Enter") {
-    //     setTimeout(() => {searchBox.value=""}, 1000);
-    // }
+	//Universals
+	switch (e.code) {
 
-    switch (e.code) {
-        case 'Enter':
-            setTimeout(() => {searchBox.value=""}, 250);
-            break;
-        case 'Backslash':
-            setTimeout(() => {searchBox.value=""}, 250);
-            break;
-    }
+		//Clears input field on search
+		case 'Enter':
+			setTimeout(() => { searchBox.value = "" }, 250);
+			break;
+
+		//Clears input field on \ for convenience
+		case 'Backslash':
+			setTimeout(() => { searchBox.value = "" }, 250);
+			break;
+	}
+
+	//Only on modifier key (vim-like)
+	if (searchBox.value.charAt(0) == ":") {
+		switch (e.code) {
+
+			//Switches provider to Google
+			case 'KeyG':
+				setTimeout(() => { searchBox.value = "" }, 500);
+				setProvider('google');
+				break;
+
+			//Switches provider to DuckDuckgo
+			case 'KeyD':
+				setTimeout(() => { searchBox.value = "" }, 500);
+				setProvider('duckduckgo');
+				break;
+
+			//Switches provider to Startpage
+			case 'KeyS':
+				setTimeout(() => { searchBox.value = "" }, 500);
+				setProvider('startpage');
+				break;
+
+			// Switches provider to Wikipedia(not working yet)
+			case 'KeyW':
+				setTimeout(() => { searchBox.value = "" }, 500);
+				setProvider('wikipedia');
+				break;
+
+			//Suppoed to change the layout, also not working
+			// case 'bento':
+			// 	CONFIG.bentoLayout = "bento";
+
+
+		}
+	}
 });
 
+//maybe add theme toggle command
+//bookmark access
+//layout change
 
-//commands
-//currently only to switch search engine, but will likely add other stuff like changing the layout
-searchBox.addEventListener('keydown', (e) => {
-        switch (e.key) {
-            case '`g':
-                setTimeout(() => {searchBox.value=""}, 500);
-                searchBox.placeholder = "Search for anything on Google";
-                get.action = "https://www.google.com/";
-                break;
+const setProvider = (provider) => {
+	switch (provider) {
+		case 'google':
+			searchBox.placeholder = "Search for anything on Google";
+			get.action = "https://www.google.com/search";
+			localStorage.setItem("defaultProvider", "google");
+			break;
 
-            case '`d':
-                setTimeout(() => {searchBox.value=""}, 500);
-                searchBox.placeholder = "Search for anything on DuckDuckgo";
-                get.action = "https://www.duckduckgo.com/";
-                break;
+		case 'duckduckgo':
+			searchBox.placeholder = "Search for anything on DuckDuckgo";
+			get.action = "https://www.duckduckgo.com/";
+			localStorage.setItem("defaultProvider", "duckduckgo");
+			break;
 
-            // case '`w':
-            //     setTimeout(() => {searchBox.value=""}, 500);
-            //     searchBox.placeholder = "Search for anything on Wikipedia";
-            //     get.action = "https://www.wikipedia.org/wiki";
-            //     searchBox.name = "";
-            //     break;
+		case 'startpage':
+			searchBox.placeholder = "Search for anything on Startpage";
+			get.action = "https://www.startpage.com/sp/search";
+			localStorage.setItem("defaultProvider", "startpage");
+			break;
 
-            case '`s':
-                setTimeout(() => {searchBox.value=""}, 500);
-                searchBox.placeholder = "Search for anything on Startpage";
-                get.action = "https://www.startpage.com/sp/search";
-                break;
+		// case 'wikipedia':
+		// 	searchBox.placeholder = "Search for anything on Wikipedia";
+		// 	get.action = "https://www.wikipedia.org/wiki";
+		// localStorage.setItem("defaultProvider", "wikipedia");
+			// break;
+	}
 
-            case '`l':
-                setTimeout(() => {searchBox.value=""}, 500);
+}
 
-                case 'bento':
-                    CONFIG.bentoLayout = "bento";
-                    
-                
-        }
-});
+setProvider(localStorage.getItem("defaultProvider"));
