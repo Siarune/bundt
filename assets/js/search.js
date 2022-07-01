574a2a9f71cc6a04313c4f30a0b00f34443b0c1e
sia <sia@siarune.dev>
2022-07-01 16:38:59 -0400

    migration to native searx

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,56 +2,72 @@ const searchBox = document.getElementById("searchtxt");
 const get = document.getElementById("phpaction");
 
 //Command controller (paste-safe)
-searchBox.addEventListener('keypress', (e) => {
-	//Universals
-	switch (e.code) {
+searchBox.addEventListener("keypress", (e) => {
+  //Universals
+  switch (e.code) {
+    //Clears input field on search
+    case "Enter":
+      setTimeout(() => {
+        searchBox.value = "";
+      }, 250);
+      break;
 
-		//Clears input field on search
-		case 'Enter':
-			setTimeout(() => { searchBox.value = "" }, 250);
-			break;
+    //Clears input field on \ for convenience
+    case "Backslash":
+      setTimeout(() => {
+        searchBox.value = "";
+      }, 250);
+      break;
+  }
 
-		//Clears input field on \ for convenience
-		case 'Backslash':
-			setTimeout(() => { searchBox.value = "" }, 250);
-			break;
-	}
+  //Only on modifier key (vim-like)
+  if (searchBox.value.charAt(0) == ":") {
+    switch (e.code) {
+      //Switches provider to Google
+      case "KeyG":
+        setTimeout(() => {
+          searchBox.value = "";
+        }, 500);
+        setProvider("google");
+        break;
 
-	//Only on modifier key (vim-like)
-	if (searchBox.value.charAt(0) == ":") {
-		switch (e.code) {
+      //Switches provider to DuckDuckgo
+      case "KeyD":
+        setTimeout(() => {
+          searchBox.value = "";
+        }, 500);
+        setProvider("duckduckgo");
+        break;
 
-			//Switches provider to Google
-			case 'KeyG':
-				setTimeout(() => { searchBox.value = "" }, 500);
-				setProvider('google');
-				break;
+      //Switches provider to Startpage
+      case "KeyS":
+        setTimeout(() => {
+          searchBox.value = "";
+        }, 500);
+        setProvider("startpage");
+        break;
 
-			//Switches provider to DuckDuckgo
-			case 'KeyD':
-				setTimeout(() => { searchBox.value = "" }, 500);
-				setProvider('duckduckgo');
-				break;
+      // Switches provider to Wikipedia(not working yet)
+      case "KeyW":
+        setTimeout(() => {
+          searchBox.value = "";
+        }, 500);
+        setProvider("wikipedia");
+        break;
 
-			//Switches provider to Startpage
-			case 'KeyS':
-				setTimeout(() => { searchBox.value = "" }, 500);
-				setProvider('startpage');
-				break;
+      // Switches provider to Wikipedia(not working yet)
+      case "KeyM":
+        setTimeout(() => {
+          searchBox.value = "";
+        }, 500);
+        setProvider("siasearx");
+        break;
 
-			// Switches provider to Wikipedia(not working yet)
-			case 'KeyW':
-				setTimeout(() => { searchBox.value = "" }, 500);
-				setProvider('wikipedia');
-				break;
-
-			//Suppoed to change the layout, also not working
-			// case 'bento':
-			// 	CONFIG.bentoLayout = "bento";
-
-
-		}
-	}
+      //Suppoed to change the layout, also not working
+      // case 'bento':
+      // 	CONFIG.bentoLayout = "bento";
+    }
+  }
 });
 
 //maybe add theme toggle command
@@ -59,32 +75,36 @@ searchBox.addEventListener('keypress', (e) => {
 //layout change
 
 const setProvider = (provider) => {
-	switch (provider) {
-		case 'google':
-			searchBox.placeholder = "Search for anything on Google";
-			get.action = "https://www.google.com/search";
-			localStorage.setItem("defaultProvider", "google");
-			break;
+  switch (provider) {
+    case "google":
+      searchBox.placeholder = "Search for anything on Google";
+      get.action = "https://www.google.com/search";
+      localStorage.setItem("defaultProvider", "google");
+      break;
 
-		case 'duckduckgo':
-			searchBox.placeholder = "Search for anything on DuckDuckgo";
-			get.action = "https://www.duckduckgo.com/";
-			localStorage.setItem("defaultProvider", "duckduckgo");
-			break;
+    case "duckduckgo":
+      searchBox.placeholder = "Search for anything on DuckDuckgo";
+      get.action = "https://www.duckduckgo.com/";
+      localStorage.setItem("defaultProvider", "duckduckgo");
+      break;
 
-		case 'startpage':
-			searchBox.placeholder = "Search for anything on Startpage";
-			get.action = "https://www.startpage.com/sp/search";
-			localStorage.setItem("defaultProvider", "startpage");
-			break;
+    case "startpage":
+      searchBox.placeholder = "Search for anything on Startpage";
+      get.action = "https://www.startpage.com/sp/search";
+      localStorage.setItem("defaultProvider", "startpage");
+      break;
 
-		// case 'wikipedia':
-		// 	searchBox.placeholder = "Search for anything on Wikipedia";
-		// 	get.action = "https://www.wikipedia.org/wiki";
-		// localStorage.setItem("defaultProvider", "wikipedia");
-			// break;
-	}
-
-}
+    case "siasearx":
+      searchBox.placeholder = "Search for anything";
+      get.action = "https://searx.siarune.dev/";
+      localStorage.setItem("defaultProvider", "siasearx");
+      break;
+    // case 'wikipedia':
+    // 	searchBox.placeholder = "Search for anything on Wikipedia";
+    // 	get.action = "https://www.wikipedia.org/wiki";
+    // localStorage.setItem("defaultProvider", "wikipedia");
+    // break;
+  }
+};
 
 setProvider(localStorage.getItem("defaultProvider"));
